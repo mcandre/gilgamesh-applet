eb8ca31fdf488f3394452cf2d0dfdcab43bdcdb3
alex.schoof <alex.schoof@98bfe466-9cda-11de-a5bb-6b039de6238a>
2009-09-13 06:59:43 +0000

    as of now it runs on its own fine, but wont add properly to the panel

--- Linux/install.sh
+++ Linux/install.sh
@@ -2,4 +2,5 @@
 
 echo "Installing Gilgamesh"
 cp gilgamesh-applet.py  /usr/local/bin/gilgamesh-applet.py
-cp gilgamesh-applet.server /usr/lib/bonobo/servers/gilgamesh.server
+cp gilgamesh-applet.server /usr/lib/bonobo/servers/GNOME_Gilgamesh.server
+bonobo-server-run-query
