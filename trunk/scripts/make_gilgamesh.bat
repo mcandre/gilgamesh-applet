7607e0daf1b626c43249521cf785b8b06a200767
andrew.pennebaker <andrew.pennebaker@98bfe466-9cda-11de-a5bb-6b039de6238a>
2009-09-15 04:49:59 +0000

    Compiled Windows script.

--- trunk/scripts/make_gilgamesh.bat
+++ trunk/scripts/make_gilgamesh.bat
@@ -1,8 +1,11 @@
 @echo off
 
-REM Ignore the following warning:
-REM
-REM Math::BigInt: couldn't load specified math lib(s),
-REM fallback to Math::BigInt::FastCalc at C:/Perl/lib/Win32API/File.pm line 20
+echo Ignore the following warning:
+echo.
+echo "Math::BigInt: couldn't load specified math lib(s),
+echo fallback to Math::BigInt::FastCalc at C:/Perl/lib/Win32API/File.pm line 20"
+echo.
+echo Error:
+echo.
 
 pp -o bin\gilgamesh.exe -B -M Error -M YAML -M Crypt::SSLeay -M WWW::Mechanize -M HTML::TokeParser gilgamesh.pl
